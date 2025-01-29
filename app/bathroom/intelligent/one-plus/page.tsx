8ec078862c34e9114e95996b7a65bcec86321180
Qiuyu Chen <qchenla@gmail.com>
2025-01-28 18:51:28 -0800

    one-plus page done

--- app/bathroom/intelligent/one-plus/page.tsx
+++ app/bathroom/intelligent/one-plus/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import ProductSketch from "@/components/ProductSketch";
 import FeatureCard from "@/components/FeatureCard";
 import { data } from "../../../../lib/data"; //all product data in lib
@@ -8,71 +8,127 @@ import PageVideo from "@/components/PageVideo";
 import MaskText from "@/components/MaskText";
 
 export default function OnePlus() {
+  const onePlusData = data[1];
+  const combinedValues = [
+    ...(onePlusData.configuration?.outlook ?? []),
+    ...(onePlusData.configuration?.watertank ?? []),
+    ...(onePlusData.configuration?.cleaning ?? []),
+    ...(onePlusData.configuration?.seatlid ?? []),
+  ];
+  return (
+    <div className="pt-20 sm:pt-16 sm:px-[10vw]">
+      <ProductSketch
+        title={onePlusData.name}
+        subtitle={onePlusData.subtitle}
+        description={onePlusData.details}
+        sketchImgs={onePlusData.sketchImg}
+      />
+      <FeatureCard
+        sku={onePlusData.sku}
+        collectionName={
+          onePlusData.collection === null ? "" : onePlusData.collection
+        }
+        productImgUrl={onePlusData.imgSrc}
+        productImgAlt="sli 1000"
+        productImgHeight={500}
+        productImgWidth={500}
+        dimensions={
+          onePlusData.features.dimensions === undefined
+            ? ""
+            : onePlusData.features.dimensions
+        }
+        weight={
+          onePlusData.features.weight === undefined
+            ? ""
+            : onePlusData.features.weight
+        }
+        finish={onePlusData.features.material}
+        color={onePlusData.features.color}
+        consumption={
+          onePlusData.features.water === null ? "" : onePlusData.features.water
+        }
+        configurations={combinedValues}
+      />
+      {/* live image and video */}
+      <div className="flex flex-col items-center justify-center font-custom font-thin text-primary text-sm sm:text-2xl">
+        <PageVideo videoUrl="/videos/one-plus-video.mp4" />
+        <MaskText
+          className="mx-5 my-5 sm:mx-20 sm:my-10"
+          phrases={[
+            "True excellence is an inherent uniqueness. The AXENT.ONE Intelligent Toilet revolutionizes convenience with its ONE.DIAL, allowing for seamless adjustment of water flow and temperature during the flushing process —as intuitive as turning the crown of a Swiss watch.",
+          ]}
+        />
 
-    const onePlusData = data[1]
-    const combinedValues = [
-        ...(onePlusData.configuration?.outlook ?? []),
-        ...(onePlusData.configuration?.watertank ?? []),
-        ...(onePlusData.configuration?.cleaning ?? []),
-        ...(onePlusData.configuration?.seatlid ?? [])
-    ];
-    return (
-        <div className="pt-20 sm:pt-16 sm:px-[20vw]">
-            <ProductSketch
-                title={onePlusData.name}
-                subtitle={onePlusData.subtitle}
-                description={onePlusData.details}
-                sketchImgs={onePlusData.sketchImg}
-            />
-            <FeatureCard
-            sku = {onePlusData.sku}
-                collectionName={onePlusData.collection === null ? "" : onePlusData.collection}
-                productImgUrl={onePlusData.imgSrc}
-                productImgAlt="sli 1000"
-                productImgHeight={500}
-                productImgWidth={500}
-                dimensions={onePlusData.features.dimensions === undefined ? "" : onePlusData.features.dimensions}
-                weight={onePlusData.features.weight === undefined ? "" : onePlusData.features.weight}
-                finish={onePlusData.features.material}
-                color={onePlusData.features.color}
-                consumption={onePlusData.features.water === null ? "" : onePlusData.features.water}
-                configurations={
-                    combinedValues
-                }
-            />
-
-            {/* live image and video */}
-            <div className="flex flex-col items-center justify-center font-custom font-thin text-primary text-sm sm:text-2xl">
-                <PageVideo videoUrl="/videos/one-c-plus-video.mp4" />
-                <MaskText className="mx-5 my-5 sm:mx-40 sm:my-10" phrases={["Merging Italian iconic design with cutting-edge technology, and equipped with Kirin Pressure-assisted Flush System, it features both water-saving efficiency and robust performance. Intelligent features such as auto open embody people-centered care in every detail, crafting an elegant and comfortable private space."]} />
-
-                <div className="flex flex-col sm:flex-row w-full justify-between sm:items-end">
-                    <Image src={"/products_img/one-c-plus-powerfullflush.png"} alt={onePlusData.name} width={600} height={400} />
-                    <MaskText className="  my-5  sm:my-10" phrases={["Air-pressurized Powerful Flush", "AXENT Kirin Pressure-assisted Flush System functions well in high-rise buildings, old construc- tions, and water usage peaks. Each Flush requires only 1 Gal of water, much lower than international standards. It flushes normally even during power outages."]} />
-                </div>
-
-                <div className="flex flex-col sm:flex-row-reverse w-full justify-between sm:items-end">
-                    <Image src={"/products_img/one-c-plus-one-dial.png"} alt={onePlusData.name} width={600} height={400} />
-                    <MaskText className=" my-5  sm:my-10" phrases={["People-centric Smart Controls", "Auto open and seat adjustment means convenience and hygiene without bending over. The ONE.DIAL simplifies operations, making it easy for the elderly and children to use."]} />
-                </div>
-
-                <div className="flex flex-col sm:flex-row w-full justify-between sm:items-end sm:text-end">
-                    <Image src={"/products_img/one-c-plus-comfort.png"} alt={onePlusData.name} width={1024} height={600} />
-                    <MaskText className="w-full sm:ml-10  my-5  sm:my-10" phrases={["Intelligent Cleansing for Ultimate Comfort", "AXENT Bubble Care Technology mixes water and air in a perfect ratio for gentle skin care. Five cleansing modes cater to diverse needs, ensuring thorough cleanliness and comfort. The four-barrier nozzle effectively eliminates bacteria, guaranteeing safe cleansing."]} />
-                </div>
-
-                <div className="flex flex-col sm:flex-row-reverse w-full justify-between sm:items-end">
-                    <Image src={"/products_img/one-c-plus-care.png"} alt={onePlusData.name} width={600} height={400} />
-                    <MaskText className=" my-5  sm:my-10" phrases={["Thoughtful Care, Just for You", "Warm air drying with a delicate touch, five-grade adjustable air temperature.With five seat temperature settings and eco-friendly seat heating functions, it autonomously learns user habits to deliver personalized warmth. Intelligent deodorization with diatomaceous earth, providing powerful purification and ensuring freshness at all times."]} />
-                </div>
-
-                <div className="flex flex-col sm:flex-row w-full justify-between sm:items-end sm:text-end">
-                    <Image src={"/products_img/one-c-plus-safe.png"} alt={onePlusData.name} width={600} height={600} />
-                    <MaskText className="w-full sm:ml-10  my-5  sm:my-10" phrases={["Safe, Convenient and Gentle Protection", "A nighttime guard with gentle light and without disruption to sleep. Wireless Dual Controls (App Control and Remote Control) put all functions at your fingertips. IPX4 waterproof and moisture-resistant features provide quadruple protection for electricity and water usage, ensuring comprehensive safety."]} />
-                </div>
-
-                <Image className="w-screen" src={"/products_img/one-c-plus-3.png"} alt={onePlusData.name} width={1024} height={600} />
-            </div>
+        <div className="flex flex-col sm:flex-row w-full justify-between sm:items-start">
+          <Image
+            src={"/products_img/one-plus-flush.png"}
+            alt={onePlusData.name}
+            width={500}
+            height={400}
+            className="w-full h-full object-cover"
+          />
+          <MaskText
+            className=" my-5  sm:my-10"
+            phrases={[
+              "AXENT.INFINITY Infinity Flush System",
+              "Powerful Flush, Ultra-quiet, Bacteria-free, Always clean, Dual-flush with First-class Water Efficiency.",
+            ]}
+          />
         </div>
-    )
+
+        <div className="pt-10 flex flex-col sm:flex-row-reverse  w-full justify-between sm:items-start">
+          <div className="basis-2/3">
+            <PageVideo videoUrl="/videos/hygienic.mp4" />
+          </div>
+          <MaskText
+            className="basis-2/3 my-5  sm:my-10"
+            phrases={[
+              "Gentle Cleansing, Intimate Understanding",
+              "Five Cleansing Modes, Warm Air Dry, Eco-friendly & Energy-saving Seat, Bubble Care.",
+            ]}
+          />
+        </div>
+
+        <div className="flex flex-col sm:flex-row w-full justify-between sm:items-end sm:text-end">
+          <Image
+            src={"/products_img/one-plus-4.png"}
+            alt={onePlusData.name}
+            width={600}
+            height={600}
+          />
+          <MaskText
+            className="w-full sm:ml-10  my-5  sm:my-10"
+            phrases={[
+              " Always Clean, Freshness Abounds",
+              "Triple-Barrier Nanotechnology, Antibacterial Nozzle. Intelligent Deodorization with Pure Diatomaceous Earth. Rimfree Ceramic Design. Dual Antibacterial Protection",
+            ]}
+          />
+        </div>
+
+        <div className="pt-10 flex flex-col sm:flex-row-reverse w-full justify-between sm:items-start">
+          <Image
+            src={"/products_img/one-c-plus-safe.png"}
+            alt={onePlusData.name}
+            width={500}
+            height={600}
+          />
+          <MaskText
+            className="w-full  my-5  sm:my-10"
+            phrases={[
+              " Wireless Dual Controls, Thoughtful Care",
+              "App control and a wireless multifunction remote control. A nighttime guard without disruption to sleep, it automatically turns on/off according to the ambient light.",
+            ]}
+          />
+        </div>
+
+        <Image
+          className="pt-10 w-screen"
+          src={"/products_img/one-plus-3.jpg"}
+          alt={onePlusData.name}
+          width={1024}
+          height={600}
+        />
+      </div>
+    </div>
+  );
 }
--- components/PageVideo.tsx
+++ components/PageVideo.tsx
@@ -1,21 +1,16 @@
-
-type Prop = { videoUrl: string; }
-
+type Prop = { videoUrl: string };
 
 export default function PageVideo({ videoUrl }: Prop) {
-
-
-
-    return (
-        <div className="relative w-full h-full overflow-hidden">
-            {/* Video for large screens (full width) */}
-            <video
-                className=" w-full h-full object-contain"
-                autoPlay
-                loop
-                muted
-                src={videoUrl}
-            />
-        </div>
-    );
+  return (
+    <div className="relative w-full h-full overflow-hidden">
+      {/* Video for large screens (full width) */}
+      <video
+        className=" w-full h-full object-contain"
+        autoPlay
+        loop
+        muted
+        src={videoUrl}
+      />
+    </div>
+  );
 }

31226c16d797d2c627fecd1f8ec3c2b7d085328e
Qiuyu Chen <qchenla@gmail.com>
2025-02-06 18:37:55 -0800

    all news updated

--- lib/newsData.tsx
+++ lib/newsData.tsx
@@ -1,18 +1,19 @@
 import { BlogInfo } from "@/types/global";
 
 export const newsData: BlogInfo[] = [
-    {
-        imgsrc: "/blog_img/axent-switzerland-to-showcase-innovative-bathroom-solutions-at-kbis-2025-in-las-vegas.jpg",
-        releaseDate: "Feb 5, 2025",
-        title:
-            "AXENT Switzerland to Showcase Innovative Bathroom Solutions at KBIS 2025 in Las Vegas",
-        location: "LAS VEGAS",
-        content: [
-            "AXENT Switzerland is excited to announce its participation in the upcoming Kitchen & Bath Industry Show (KBIS) 2025, scheduled from February 25 to 27 at the Las Vegas Convention Center. Attendees are invited to visit AXENT at Booth N2271 to explore the latest advancements in intelligent bathroom technology.",
-        ],
-        pagelink:
-            "axent-switzerland-to-showcase-innovative-bathroom-solutions-at-kbis-2025-in-las-vegas",
-        newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
+  {
+    imgsrc:
+      "/blog_img/axent-switzerland-to-showcase-innovative-bathroom-solutions-at-kbis-2025-in-las-vegas.jpg",
+    releaseDate: "Feb 5, 2025",
+    title:
+      "AXENT Switzerland to Showcase Innovative Bathroom Solutions at KBIS 2025 in Las Vegas",
+    location: "LAS VEGAS",
+    content: [
+      "AXENT Switzerland is excited to announce its participation in the upcoming Kitchen & Bath Industry Show (KBIS) 2025, scheduled from February 25 to 27 at the Las Vegas Convention Center. Attendees are invited to visit AXENT at Booth N2271 to explore the latest advancements in intelligent bathroom technology.",
+    ],
+    pagelink:
+      "axent-switzerland-to-showcase-innovative-bathroom-solutions-at-kbis-2025-in-las-vegas",
+    newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
       <div className="title pt-20 font-extrabold text-3xl ">
   
         AXENT Switzerland to Showcase Innovative Bathroom Solutions at KBIS 2025 in Las Vegas
@@ -34,46 +35,188 @@ export const newsData: BlogInfo[] = [
         <img src="/blog_img/axent-switzerland-to-showcase-innovative-bathroom-solutions-at-kbis-2025-in-las-vegas_2.jpg" alt="cover image" className="w-full h-full object-contain" />
       </div>
     </div>`,
-    },
-    {
-        imgsrc: "/blog_img/kbis2024.png",
-        releaseDate: "Feb 27, 2024",
-        title:
-            "AXENT Switzerland to Unveil Cutting-Edge Bathroom Innovations at KBIS 2025 in Las Vegas",
-        location: "LAS VEGAS",
-        content: [
-            "The Kitchen & Bath Industry Show (KBIS) 2024, held in Las Vegas, Nevada, marked a significant milestone as the largest annual kitchen and bath event to date. The show featured a record-breaking number of exhibitors and attendees, highlighting the industry's latest innovations and trends. ",
-        ],
-        pagelink:
-            "axent-switzerland-to-unveil-cutting-edge-bathroom-innovations-at-kbis-2024-in-las-vegas",
-        newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
-    <div className="title pt-20 font-extrabold text-3xl ">
-
-      AXENT Switzerland to Unveil Cutting-Edge Bathroom Innovations at KBIS 2024 in Las Vegas
-    </div>
-
-    <div className="cover-img pt-10 sm:w-full w-screen flex flex-col items-center ">
-      <img src="/blog_img/kbis2024.png" alt="cover image" className="w-full h-full object-contain" />
-    </div>
-
-    <div className="news-content pt-10 font-thin text-lg">
-      The Kitchen & Bath Industry Show (KBIS) 2024, held in Las Vegas, Nevada, marked a significant milestone as the largest annual kitchen and bath event to date. The show featured a record-breaking number of exhibitors and attendees, highlighting the industry's latest innovations and trends.
-    </div>
-
-    <div className="flex justify-center font-custom text-primary ">
-      <div className="aspect-w-16 aspect-h-9 sm:w-96 w-screen">
-        <iframe src="https://player.vimeo.com/video/1054334050?h=775685778c" frameBorder="0" allow="fullscreen"></iframe>
+  },
+  {
+    imgsrc: "/blog_img/kbis2024.png",
+    releaseDate: "Feb 27, 2024",
+    title:
+      "AXENT Switzerland to Unveil Cutting-Edge Bathroom Innovations at KBIS 2025 in Las Vegas",
+    location: "LAS VEGAS",
+    content: [
+      "The Kitchen & Bath Industry Show (KBIS) 2024, held in Las Vegas, Nevada, marked a significant milestone as the largest annual kitchen and bath event to date. The show featured a record-breaking number of exhibitors and attendees, highlighting the industry's latest innovations and trends. ",
+    ],
+    pagelink:
+      "axent-switzerland-to-unveil-cutting-edge-bathroom-innovations-at-kbis-2024-in-las-vegas",
+    newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
+      <div className="title pt-20 font-extrabold text-3xl ">
+        AXENT Switzerland to Unveil Cutting-Edge Bathroom Innovations at KBIS
+        2024 in Las Vegas
       </div>
-    </div>
-    <div className="news-content pt-10 font-thin text-lg">
-      Building on the enthusiasm from previous years, AXENT returns to KBIS with a range of innovative products designed to enhance modern living. Among the highlights is the award-winning AXENT.ONE C Plus intelligent toilet, which offers features such as rear and front cleansing, adjustable seat warming, warm-air drying, an intuitive nightlight, and a convenient remote control. Users can also operate the toilet via a mobile app, providing three distinct methods of control—a notable advantage over competitors.
 
+      <div className="cover-img pt-10 sm:w-full w-screen flex flex-col items-center ">
+        <img
+          src="/blog_img/kbis2024.png"
+          alt="cover image"
+          className="w-full h-full object-contain"
+        />
+      </div>
 
-    </div>
-    <div className="news-content pt-10 font-thin text-lg">
-      AXENT's participation in KBIS 2024 underscores its dedication to expanding its presence in the North American market. The company anticipates that the exposure and connections made during the show will lead to increased sales and market share across the region.
-    </div>
-  </div>`,
-    },
+      <div className="news-content pt-10 font-thin text-lg">
+        The Kitchen & Bath Industry Show (KBIS) 2024, held in Las Vegas, Nevada,
+        marked a significant milestone as the largest annual kitchen and bath
+        event to date. The show featured a record-breaking number of exhibitors
+        and attendees, highlighting the industry's latest innovations and
+        trends.
+      </div>
 
-]
+      <div className="pt-10 flex justify-center font-custom text-primary">
+        <div className="aspect-w-16 aspect-h-9 sm:w-[60vw] w-screen">
+          <iframe
+            src="https://player.vimeo.com/video/1054334050?h=775685778c"
+            frameBorder="0"
+            allow="fullscreen"
+          ></iframe>
+        </div>
+      </div>
+      <div className="news-content pt-10 font-thin text-lg">
+        Building on the enthusiasm from previous years, AXENT returns to KBIS
+        with a range of innovative products designed to enhance modern living.
+        Among the highlights is the award-winning AXENT.ONE C Plus intelligent
+        toilet, which offers features such as rear and front cleansing,
+        adjustable seat warming, warm-air drying, an intuitive nightlight, and a
+        convenient remote control. Users can also operate the toilet via a
+        mobile app, providing three distinct methods of control—a notable
+        advantage over competitors.
+      </div>
+      <div className="news-content pt-10 font-thin text-lg">
+        AXENT's participation in KBIS 2024 underscores its dedication to
+        expanding its presence in the North American market. The company
+        anticipates that the exposure and connections made during the show will
+        lead to increased sales and market share across the region.
+      </div>
+    </div>`,
+  },
+  {
+    imgsrc: "/blog_img/palm_springs_modernism_show.png",
+    releaseDate: "July, 2023",
+    title: "AXENT Switzerland Debuts at 2023 Palm Springs Modernism Show",
+    location: "Palm Springs, California",
+    content: [
+      "AXENT Switzerland made its inaugural appearance at the Palm Springs Modernism Show, a prominent event celebrating mid-century modern architecture and design. ",
+    ],
+    pagelink: "axent-switzerland-debuts-at-2023-palm-springs-modernism-show",
+    newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
+      <div className="title pt-20 font-extrabold text-3xl ">
+        AXENT Switzerland Debuts at 2023 Palm Springs Modernism Show
+      </div>
+      <div className="pt-10 flex justify-center font-custom text-primary">
+        <div className="aspect-w-16 aspect-h-9 sm:w-[80vw] w-screen">
+          <iframe
+            src="https://player.vimeo.com/video/1054352184"
+            frameBorder="0"
+            allow="fullscreen"
+          ></iframe>
+        </div>
+      </div>
+
+      <div className="news-content pt-10 font-thin text-lg">
+        AXENT Switzerland made its inaugural appearance at the Palm Springs
+        Modernism Show, a prominent event celebrating mid-century modern
+        architecture and design. The show, held during Modernism Week, featured
+        125 premier national and international exhibitors presenting vintage
+        furniture, decor, art, and jewelry reflecting 20th-century design
+        movements.
+      </div>
+
+      <div className="news-content pt-10 font-thin text-lg">
+        AXENT's presence at the show provided an opportunity to engage with
+        design enthusiasts and industry professionals, showcasing its products
+        in a setting that honors both historical and contemporary design
+        innovations.
+      </div>
+    </div>`,
+  },
+  {
+    imgsrc: "/blog_img/kbis2023.jpg",
+    releaseDate: "Feb, 2023",
+    title:
+      "AXENT Switzerland Showcases Innovative Bathroom Solutions at KBIS 2023",
+    location: "LAS VEGAS",
+    content: [
+      "AXENT Switzerland made a notable appearance at the Kitchen & Bath Industry Show (KBIS) 2023, held in Las Vegas, Nevada. The company showcased its innovative bathroom solutions, highlighting its commitment to design excellence and technological advancement.",
+    ],
+    pagelink:
+      "axent-switzerland-showcases-innovative-bathroom-solutions-at-kbis-2023",
+    newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
+      <div className="title pt-20 font-extrabold text-3xl ">
+        AXENT Switzerland Showcases Innovative Bathroom Solutions at KBIS 2023
+      </div>
+      <div className="pt-10 flex justify-center font-custom text-primary">
+        <div className="aspect-w-16 aspect-h-9 sm:w-[80vw] w-screen">
+          <iframe
+            src="https://player.vimeo.com/video/1054353382"
+            frameBorder="0"
+            allow="fullscreen"
+          ></iframe>
+        </div>
+      </div>
+      <div className="news-content pt-10 font-thin text-lg">
+        AXENT Switzerland made a notable appearance at the Kitchen & Bath
+        Industry Show (KBIS) 2023, held in Las Vegas, Nevada. The company
+        showcased its innovative bathroom solutions, highlighting its commitment
+        to design excellence and technological advancement.
+      </div>
+
+      <div className="news-content pt-10 font-thin text-lg">
+        AXENT's presence at KBIS 2023 reinforced its position as a leader in the
+        bathroom fixtures industry, showcasing its latest innovations to a
+        diverse audience of industry professionals and design enthusiasts.
+      </div>
+    </div>`,
+  },
+  {
+    imgsrc: "/blog_img/icff2022.png",
+    releaseDate: "May 15, 2022",
+    title:
+      "AXENT Switzerland Showcases Innovative Bathroom Solutions at ICFF 2022 in New York",
+    location: "NEW YORK",
+    content: [
+      "AXENT Switzerland participated in the International Contemporary Furniture Fair (ICFF) in New York City, showcasing its innovative bathroom solutions to a global audience. The event, held at the Javits Center, is renowned for highlighting contemporary design and serves as a premier platform for international brands.",
+    ],
+    pagelink:
+      "axent-switzerland-showcases-innovative-bathroom-solutions-at-icff-2022-in-new-york",
+    newsPage: `<div className="news-container sm:px-60 px-5 sm:pt-16 pt-20 font-custom text-primary flex flex-col justify-center items-center">
+      <div className="title pt-20 font-extrabold text-3xl ">
+        AXENT Switzerland Showcases Innovative Bathroom Solutions at ICFF 2022
+        in New York
+      </div>
+      <div className="pt-10 flex justify-center font-custom text-primary">
+        <div className="aspect-w-9 aspect-h-16 sm:w-[30vw] w-screen">
+          <iframe
+            src="https://player.vimeo.com/video/1054354106"
+            frameBorder="0"
+            allow="fullscreen"
+          ></iframe>
+        </div>
+      </div>
+
+      <div className="news-content pt-10 font-thin text-lg">
+        AXENT Switzerland participated in the International Contemporary
+        Furniture Fair (ICFF) in New York City, showcasing its innovative
+        bathroom solutions to a global audience. The event, held at the Javits
+        Center, is renowned for highlighting contemporary design and serves as a
+        premier platform for international brands.
+      </div>
+
+      <div className="news-content pt-10 font-thin text-lg">
+        At ICFF, AXENT introduced its latest advancements in bathroom
+        technology, including the AXENT.One C and AXENT.One wall-hung toilets.
+        These products feature tankless designs and offer functionalities such
+        as rear and female wash, adjustable water temperature and flow rate,
+        oscillating spray, warm air drying, seat heating, deodorization, and a
+        night light. Users can control these features via a discreet remote
+        control or through AXENT's dedicated smartphone app.
+      </div>
+    </div>`,
+  },
+];
